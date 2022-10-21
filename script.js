97e571e663af98039633f6aab4df56ac89557de3
Dave Hughes <hughesd11@gmail.com>
2022-10-20 21:35:39 -0400

    Complete API call using coded input

---
+++ script.js
@@ -0,0 +1,41 @@
+const app = {};
+
+const pokemon = "charizard";
+
+//next step is to conenct input box from HTML to our API call using 'pokemon' const
+
+const baseUrl = "https://pokeapi.co/api/v2/pokemon/";
+
+app.init = () => {
+  app.getApp();
+};
+
+app.getApp = () => {
+  fetch(`${baseUrl}${pokemon}`)
+    .then((res) => {
+      console.log(res);
+      if (res.ok === true) {
+        return res.json();
+      } else {
+        throw new Error(res.statusText);
+      }
+    })
+    .then((pokemonInfo) => {
+      const imgSrc = pokemonInfo.sprites.front_default;
+      const imgContainer = document.querySelector(".imageContainer");
+      const img = document.createElement("img");
+      img.src = imgSrc;
+      img.alt = `picture of ${pokemonInfo.name}`;
+      imgContainer.appendChild(img);
+    })
+    .catch((error) => {
+      console.log(error);
+      if (error.message === "Not Found") {
+        alert("pick a pokemon that exists! use a new number or name.");
+      } else {
+        alert("something is wrong");
+      }
+    });
+};
+
+app.init();
