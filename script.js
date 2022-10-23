f26c39b0724e9941806f0533757ff3e4b0ab06df
Jie eun Lee <jieeunlee91@gmail.com>
2022-10-23 14:59:08 -0400

    Finished input result to show image in container after button click (#6)
    Input value shows inside imageContainer after clicking button

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
 const app = {};
 
-// const pokemon = "charizard";
-
-//next step is to conenct input box from HTML to our API call using 'pokemon' const
-
 const baseUrl = "https://pokeapi.co/api/v2/pokemon/";
 
-// app.init = () => {
-//   // app.getApp();
-// };
+// grab button from index.html
+const button = document.getElementById("getPokemon");
 
-let pokemon = document.getElementById("pokemonInput").value;
+app.init = () => {
+  // add click event to the button 
+  button.addEventListener('click', e => {
+  // grab input from index.html  
+  const pokemonInput = document.getElementById("pokemonInput");
+  // print search result from input value
+    app.getApp(pokemonInput.value);
+  })
+};
 
-// console.log(pokemon);
+app.getApp = (pokemon) => {
 
-const btn = document.getElementById("getPokemon");
-
-btn.addEventListener(`click`, function (event) {
-  app.getApp();
-});
-
-app.getApp = () => {
   fetch(`${baseUrl}${pokemon}`)
     .then((res) => {
       console.log(res);
@@ -34,6 +30,8 @@ app.getApp = () => {
       const imgSrc = pokemonInfo.sprites.front_default;
       const imgContainer = document.querySelector(".imageContainer");
       const img = document.createElement("img");
+      // clear inside image container 
+      imgContainer.innerHTML = ``;
       img.src = imgSrc;
       img.alt = `picture of ${pokemonInfo.name}`;
       imgContainer.appendChild(img);
@@ -41,13 +39,11 @@ app.getApp = () => {
     .catch((error) => {
       console.log(error);
       if (error.message === "Not Found") {
-        alert("pick a pokemon that exists! use a new number or name.");
+        alert("pick a pokemon that exists! input the correct pokemon name.");
       } else {
         alert("something is wrong");
       }
     });
-
-  pokemon.value = "";
 };
 
 // app.init();
