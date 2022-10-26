65c9d8eb538332c5e81f14872f7d7f5c489c8336
jlo <77813132+jasmine-lmj@users.noreply.github.com>
2022-10-25 20:53:26 -0400

    Finished adding ugly pokemon name and updated alert message (#8)

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const button = document.getElementById("getPokemon");
 // grab uglyRandomButton from index.html
 const uglyPokemonButton = document.getElementById("uglyRandom");
 // console.log(uglyPokemonButton)
+const uglyName = document.getElementById("uglyName");
 
 const uglyPokemonList = [ 
 "dracovish",  
@@ -36,52 +37,55 @@ app.init = () => {
   app.inputResult();
 };
 
-app.inputResult = () => {
-    // add click event to the button 
-  button.addEventListener('click', e => {  
-  // print search result from input value
-    app.getApp(pokemonInput.value);
-    // app.getUglyPokemon();
-  })
-}
 app.getUglyPokemon = () => {
   uglyPokemonButton.addEventListener('click', e => {
     // create a variable to get random result from the array
     let result = uglyPokemonList[Math.floor(Math.random() * uglyPokemonList.length)];
-    console.log(result);
+    uglyName.textContent = `${result}`;
     // link button result to the api call to show image inside image container
     app.getApp(result);
   })
 }
 
+app.inputResult = () => {
+    // add click event to the button 
+  button.addEventListener('click', e => {  
+  // print search result from input value
+    uglyName.textContent = '';
+    app.getApp(pokemonInput.value);
+    // app.getUglyPokemon();
+  })
+}
+
+
 app.getApp = (pokemon) => {
   fetch(`${baseUrl}${pokemon}`)
     .then((res) => {
-      console.log(res);
-      if (res.ok === true) {
-        return res.json();
-      } else {
-        throw new Error(res.statusText);
-      }
+    console.log(res);
+    if (res.ok === true) {
+    return res.json();
+    } else {
+    throw new Error(res.statusText);
+    }
     })
     .then((pokemonInfo) => {
-      const imgSrc = pokemonInfo.sprites.front_default;
-      const imgContainer = document.querySelector(".imageContainer");
-      const img = document.createElement("img");
-      // clear inside image container 
-      imgContainer.innerHTML = '';
-      pokemonInput.value = '';
-      img.src = imgSrc;
-      img.alt = `picture of ${pokemonInfo.name}`;
-      imgContainer.appendChild(img);
+    const imgSrc = pokemonInfo.sprites.front_default;
+    const imgContainer = document.querySelector(".imageContainer");    
+    const img = document.createElement("img");
+    // clear inside image container 
+    imgContainer.innerHTML = '';
+    pokemonInput.value = '';
+    img.src = imgSrc;
+    img.alt = `picture of ${pokemonInfo.name}`;
+    imgContainer.appendChild(img);
     })
     .catch((error) => {
-      console.log(error);
-      if (error.message === "Not Found") {
-        alert("pick a pokemon that exists! input the correct pokemon name.");
-      } else {
-        alert("something is wrong");
-      }
+    console.log(error);
+    if (error.message === "Not Found") {
+    alert("Pick a pokemon that exists! Input the correct pokemon name.");
+    } else {
+    alert("Waiting for your input!");
+    }
     });
 };
 
